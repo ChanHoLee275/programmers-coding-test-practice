54fc7f3e81a490b08e1e5b06bda64261d550ae31
이찬호 <chanholee275@gmail.com>
2021-11-05 21:23:15 +0900

    level2 풀이 변경

--- level2/2개-이하로-다른-비트.py
+++ level2/2개-이하로-다른-비트.py
@@ -1,41 +1,22 @@
-# test case 10, 11을 통과하지 못함. 이는 시간이 오래 걸려서 그러는 것이므로, 이에 대한 최적화 필요
-
-def compare(item1,item2):
-    count = 0
-    item1 = item1[::-1]
-    item2 = item2[::-1]
-    if len(item1) > len(item2):
-        for i in range(len(item1)):
-            if i < len(item2) and item1[i] != item2[i]:
-                count += 1
-            elif i >= len(item2):
-                if item1[i] != "0":
-                    count += 1
-    elif len(item1) == len(item2):
-        for i in range(len(item1)):
-            if item1[i] != item2[i]:
-                count += 1
-    else :
-        for i in range(len(item2)):
-            if i < len(item1) and item1[i] != item2[i]:
-                count += 1
-            elif i >= len(item1):
-                if item2[i] != "0":
-                    count += 1
-    return count
+def diff(number):
+    target = list(bin(number)[2:])[::-1]
+    target.append("0")
+    for i in range(len(target) - 1):
+        part = "".join(target[i:i+2])
+        if part == "00":
+            target[i] = "1"
+            return "".join(target[::-1])
+        elif part == "01":
+            target[i] = "1"
+            return "".join(target[::-1])
+        elif part == "10":
+            target[i] = "0"
+            target[i+1] = "1"
+            return "".join(target[::-1])
+            
 
 def solution(numbers):
     answer = []
-    for i in range(len(numbers)):
-        item = numbers[i]
-        bin_item = list(bin(item)[2:])
-        addition = 1
-        while True:
-            diff = compare(bin_item,list(bin(item+addition)[2:]))
-            if diff <= 2:
-                answer.append(item + addition)
-                break
-            else :
-                addition += 1
-        
+    for i in numbers:
+        answer.append(int(diff(i),2))
     return answer
